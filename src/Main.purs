5cae62565ffceabeae6a565c7436464e6b0ae46b
Tomasz Rybarczyk <paluho@gmail.com>
2018-03-22 08:32:34 +0100

    Add Alt instance - we really need generic solution here!

--- src/Main.purs
+++ src/Main.purs
@@ -2,20 +2,21 @@ module Main where
 
 import Prelude
 
+import Control.Alt (class Alt, (<|>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Data.Array (elem)
 import Data.Int (fromString)
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Monoid (class Monoid, mempty)
-import Data.Newtype (class Newtype)
+import Data.Newtype (class Newtype, unwrap)
 import Data.String (Pattern(..), dropWhile, stripPrefix, takeWhile, toCharArray)
 import Global.Unsafe (unsafeStringify)
 
 -- | __D__ from diverging as a' can diverge from a
 newtype BoomBoomD tok a' a = BoomBoomD
   { prs ∷ tok → Maybe { a ∷ a, tok ∷ tok }
-  , ser ∷ a' → tok
+  , ser ∷ a' → Maybe tok
   }
 derive instance newtypeBoomBoomD ∷ Newtype (BoomBoomD tok a' a) _
 derive instance functorBoomBoomD ∷ Functor (BoomBoomD tok a')
@@ -25,6 +26,11 @@ diverge d (BoomBoom (BoomBoomD { prs, ser })) = BoomBoomD { prs, ser: d >>> ser 
 
 infixl 5 diverge as >-
 
+divergeMaybe ∷ ∀ a a' tok. (a' → Maybe a) → BoomBoom tok a → BoomBoomD tok a' a
+divergeMaybe d (BoomBoom (BoomBoomD { prs, ser })) = BoomBoomD { prs, ser: d >=> ser }
+
+infixl 5 divergeMaybe as >?
+
 instance applyBoomBoomD ∷ (Semigroup tok) ⇒ Apply (BoomBoomD tok a') where
   apply (BoomBoomD b1) (BoomBoomD b2) = BoomBoomD { prs, ser }
     where
@@ -32,21 +38,31 @@ instance applyBoomBoomD ∷ (Semigroup tok) ⇒ Apply (BoomBoomD tok a') where
       { a: f, tok: t' } ← b1.prs t
       { a, tok: t'' } ← b2.prs t'
       pure { a: f a, tok: t'' }
-    ser a' = b1.ser a' <> b2.ser a'
+    ser a' = b1.ser a' >>= \v → ((v <> _) <$> b2.ser a')
 
 instance applicativeBoomBoomD ∷ (Monoid tok) ⇒ Applicative (BoomBoomD tok a') where
-  pure a = BoomBoomD { prs: pure <<< const { a, tok: mempty }, ser: const mempty }
+  pure a = BoomBoomD { prs: pure <<< const { a, tok: mempty }, ser: const (Just mempty) }
 
--- instance altBoomBoomD ∷ Alt (BoomBoomD tok a') where
-
+instance altBoomBoom ∷ (Semigroup tok) ⇒ Alt (BoomBoomD tok a') where
+  alt (BoomBoomD b1) (BoomBoomD b2) = BoomBoomD { prs, ser }
+    where
+    prs tok = case b1.prs tok of
+      Nothing → b2.prs tok
+      r → r
+    ser a = case b1.ser a of
+      Nothing → b2.ser a
+      r → r
 
 newtype BoomBoom tok a = BoomBoom (BoomBoomD tok a a)
 derive instance newtypeBoomBoom ∷ Newtype (BoomBoom tok a) _
 
+prefix ∷ ∀ a. String → BoomBoom String a → BoomBoomD String a a
+prefix s b = lit s *> unwrap b
+
 int ∷ BoomBoom String Int
 int = BoomBoom $ BoomBoomD $
   { prs: \t → {a: _, tok: _ } <$> (fromString <<< takeDigits $ t) <@> stripDigits t
-  , ser: show
+  , ser: show >>> Just
   }
   where
   isDigit = (_ `elem` (toCharArray "0123456789"))
@@ -56,13 +72,13 @@ int = BoomBoom $ BoomBoomD $
 lit ∷ ∀ a. String → BoomBoomD String a Unit
 lit s = BoomBoomD $
   { prs: \t → {a: unit, tok: _} <$> stripPrefix (Pattern s) t
-  , ser: const s
+  , ser: const (Just s)
   }
 
 parse ∷ ∀ a tok. BoomBoom tok a → (tok → Maybe a)
 parse (BoomBoom (BoomBoomD { prs })) = (_.a <$> _) <$> prs
 
-serialize ∷ ∀ a tok. BoomBoom tok a → (a → tok)
+serialize ∷ ∀ a tok. BoomBoom tok a → (a → Maybe tok)
 serialize (BoomBoom (BoomBoomD { ser })) = ser
 
 path :: BoomBoom String { x :: Int, y :: Int }
@@ -72,7 +88,27 @@ path = BoomBoom $
     <* lit "test"
     <*> (_.y >- int)
 
+data Three = Zero | One Int | Two Int Int
+
+one = One <$> (unone >? int)
+  where
+  unone (One i) = Just i
+  unone _ = Nothing
+
+two = Two <$> (untwo' >? int) <*> (untwo'' >? int)
+  where
+  untwo' (Two i _) = Just i
+  untwo' _ = Nothing
+  untwo'' (Two _ i) = Just i
+  untwo'' _ = Nothing
+
+three = BoomBoom $
+  (lit "one" *> one) <|> (lit "two" *> two) <|> (lit "zero" *> pure Zero)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log $ unsafeStringify (parse path "8080test200")
-  log (serialize path { x: 300, y: 800 })
+  logShow (serialize path { x: 300, y: 800 })
+  logShow (serialize three (Two 8 9))
+  logShow (serialize three Zero)
+  logShow (serialize three (One 8))
